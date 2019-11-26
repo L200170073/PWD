c171c8e0de5384b4d691413b876710f3834e2f0c
L200170073 <48116170+L200170073@users.noreply.github.com>
2019-11-26 02:02:09 -0800

    Add files via upload

---
+++ Modul 9/member.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Member</title>
+    <link rel="stylesheet" href="page.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center>
+    	<h1>Selamat Datang di Laman Member</h1>
+    	<h2>WKWKWKWKWKW.COM</h2>
+    	<a href="logouttugas.php" class="link"><u>Metu</u></a>
+    </center>
+</body>
+</html>
---
+++ Modul 9/user.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Informatika
+{
+    #region User
+    public class User
+    {
+        #region Member Variables
+        protected string _username;
+        protected string _password;
+        protected string _nama;
+        protected string _status;
+        #endregion
+        #region Constructors
+        public User() { }
+        public User(string password, string nama, string status)
+        {
+            this._password=password;
+            this._nama=nama;
+            this._status=status;
+        }
+        #endregion
+        #region Public Properties
+        public virtual string Username
+        {
+            get {return _username;}
+            set {_username=value;}
+        }
+        public virtual string Password
+        {
+            get {return _password;}
+            set {_password=value;}
+        }
+        public virtual string Nama
+        {
+            get {return _nama;}
+            set {_nama=value;}
+        }
+        public virtual string Status
+        {
+            get {return _status;}
+            set {_status=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
